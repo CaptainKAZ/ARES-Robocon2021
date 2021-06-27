e1f61721c08ae3679c10ee3324355478d7e5548b
源 <73240868+JustinLiang522@users.noreply.github.com>
2021-06-27 23:01:10 +0800

    6.27

--- ARES/Application/quick_test_task/fric.c
+++ ARES/Application/quick_test_task/fric.c
@@ -6,32 +6,33 @@ Motor *testMotor[4];
 
 #define HARDSOFT_THRESHOLD (600.0f)
 #define FRICTIONGEAR_RADIUS (0.04f)
-#define CLAMP_ANGLE (-1.85f)
+#define CLAMP_ANGLE (-2.0f)
 
-uint8_t touchFlag      = 0;
-fp32    friction0Speed = 0;
-fp32    friction1Speed = 0;
-fp32    shootSpeed     = 10.0f;
+uint8_t    touchFlag      = 0;
+const fp32 error          = 0.51081562;
+fp32       spc_ang        = 0;
+fp32       friction0Speed = 0;
+fp32       friction1Speed = 0;
+fp32       shootSpeed     = 11.0f;
 
-PID_Controller motor_angle_pid;
-
-PID_ControllerParam motor_angle_pidparam = {.general  = PIDPOS_CONTROLLER,
+PID_ControllerParam gimbal_speed_pidparam = {.general  = PIDPOS_CONTROLLER,
                                             .Int_type = NORMAL_INT,
-                                            .kP       = 30000.0f,
-                                            .kI       = 50000.0f,
-                                            .kD       = 0.3f,
+                                            .kP       = 250.0f,
+                                            .kI       = 100.0f,
+                                            .kD       = 0.0f,
                                             .kB       = 0.3f,
                                             .max_Iout = 5000,
-                                            .N        = 0};
+                                            .N        = 100};
 
-static fp32                motor_angle_O_Hlim      = 16000;
-static fp32                motor_angle_O_Llim      = -16000;
-static fp32                motor_angle_I_loop_Llim = 0;
-static fp32                motor_angle_I_loop_Hlim = 0;
-static ControllerConstrain motor_angle_constrain   = {.I_loop_Hlim = &motor_angle_I_loop_Hlim,
-                                                    .I_loop_Llim = &motor_angle_I_loop_Llim,
-                                                    .O_Hlim      = &motor_angle_O_Hlim,
-                                                    .O_Llim      = &motor_angle_O_Llim};
+PID_ControllerParam gimbal_angle_pidparam = {.general  = PIDPOS_CONTROLLER,
+                                            .Int_type = NORMAL_INT,
+                                            .kP       = 900.0f,
+                                            .kI       = 0.0f,
+                                            .kD       = 40.0f,
+                                            .kB       = 0.3f,
+                                            .max_Iout = 5000,
+                                            .N        = 50};
+
 
 static fp32         ang;
 static fp32         set;
@@ -55,7 +56,7 @@ PID_ControllerParam friction_speed_pidparam_hard = {.general  = PIDPOS_CONTROLLE
 
 PID_ControllerParam clamp_speed_pidparam = {.general  = PIDPOS_CONTROLLER,
                                             .Int_type = NORMAL_INT,
-                                            .kP       = 5.0f,
+                                            .kP       = 6.0f,
                                             .kI       = 0.0f,
                                             .kD       = 0.0f,
                                             .kB       = 0.03f,
@@ -64,7 +65,7 @@ PID_ControllerParam clamp_speed_pidparam = {.general  = PIDPOS_CONTROLLER,
 
 PID_ControllerParam clamp_angle_pidparam = {.general  = PIDPOS_CONTROLLER,
                                             .Int_type = BACK_CALCULATION_INT,
-                                            .kP       = 8000.0f,
+                                            .kP       = 10000.0f,
                                             .kI       = 0.0f,
                                             .kD       = 0.0f,
                                             .kB       = 0.3f,
@@ -113,8 +114,12 @@ void    quick_test_task() {
     testMotor[3] = CAN_Find_Motor(RM_MOTOR, INTERNAL_CAN2, 7);
     vTaskDelay(10);
   }
-  PID_ControllerInit(&motor_angle_pid, &motor_angle_constrain, &motor_angle_pidparam, 10);
-  Motor_SetAnglePID(testMotor[3], &motor_angle_pidparam);
+  
+  feedback_register(&testMotor[0]->status.speed,6);
+  feedback_register(&testMotor[1]->status.speed,7);
+  
+  Motor_SetSpeedPID(testMotor[3], &gimbal_speed_pidparam);
+  Motor_SetAnglePID(testMotor[3], &gimbal_angle_pidparam);
   Motor_SetAnglePID(testMotor[2], &clamp_angle_pidparam);
   Motor_SetSpeedPID(testMotor[2], &clamp_speed_pidparam);
   Motor_SetAltController(testMotor[0], NULL, &friction0Speed, frictionGearControl);
@@ -122,11 +127,23 @@ void    quick_test_task() {
   Motor_SetAltController(testMotor[2], NULL, NULL, clampMotorControl);
   for (;;) {
     if (sbus.real.channel[SBUS_SA_CHANNEL] > 0.8f) {
-      ang        = (testMotor[3]->status.cumulative_turn * 2 * PI + testMotor[3]->status.angle - testMotor[3]->status.zero);
-      set        = sbus.real.channel[SBUS_V1_CHANNEL] * PI;
-      fp32 speed = controllerUpdate((Controller *)&motor_angle_pid, &set, &ang, NULL);
-      Motor_SetSpeed(testMotor[3], speed, 200);
+
+      // 30度射箭
+      if (sbus.real.channel[SBUS_SB_CHANNEL] == 1)
+        spc_ang = PI / 6;
+      else
+        spc_ang = 0;
+    
+
       if (zero) {
+        // v1旋钮实现36度的微调
+        set = -sbus.real.channel[SBUS_V1_CHANNEL] * PI / 10 + error + spc_ang;
+        Motor_SetAngle(testMotor[3],set,200);
+        // ang = (testMotor[3]->status.cumulative_turn * 2 * PI + testMotor[3]->status.angle - testMotor[3]->status.zero);
+        // set = -sbus.real.channel[SBUS_V1_CHANNEL] * PI / 10 + ERROR;
+        // fp32 speed = controllerUpdate((Controller *)&motor_angle_pid, &set, &ang, NULL);
+        // Motor_SetSpeed(testMotor[3], speed, 200);
+
         friction0Speed = -shootSpeed * 60.0f / (2.0f * PI * FRICTIONGEAR_RADIUS);
         friction1Speed = shootSpeed * 60.0f / (2.0f * PI * FRICTIONGEAR_RADIUS);
         if (sbus.real.channel[SBUS_SD_CHANNEL] > 0.8f && timebase <= 0) {
@@ -144,6 +161,7 @@ void    quick_test_task() {
           Motor_AltControl(testMotor[2], 100);
         }
       } else {
+
         friction0Speed = friction1Speed = 0;
         Motor_SetCurrent(testMotor[2], 2000, 100);
         if (testMotor[2]->status.speed == 0) {
